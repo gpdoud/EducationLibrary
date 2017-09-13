0d22bf880fd2ab0cbdd78f4aee6f392c59bedd7b
Greg doud <gpdoud@gmail.com>
2017-09-13 12:30:05 -0400

    Initial commit

---
+++ EducationLibrary/StudentCollection.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationLibrary {
+
+	public class StudentCollection : List<Student> {
+
+		public static StudentCollection Select() {
+			var connStr = @"Server=DSI-WORKSTATION\SQLEXPRESS;Database=DotNetDatabase;Trusted_Connection=yes;";
+			SqlConnection connection = new SqlConnection(connStr);
+			connection.Open();
+			if(connection.State != System.Data.ConnectionState.Open) {
+				Console.WriteLine("Connection did not open.");
+				return null;
+			}
+			StudentCollection students = new StudentCollection();
+			var sql = "Select * from Student";
+			SqlCommand cmd = new SqlCommand(sql, connection);
+			SqlDataReader reader = cmd.ExecuteReader();
+			while(reader.Read()) {
+				var id = reader.GetInt32(reader.GetOrdinal("Id"));
+				var firstName = reader.GetString(reader.GetOrdinal("FirstName"));
+				var lastName = reader.GetString(reader.GetOrdinal("LastName"));
+				var address = reader.GetString(reader.GetOrdinal("Address"));
+				var city = reader.GetString(reader.GetOrdinal("City"));
+				var state = reader.GetString(reader.GetOrdinal("State"));
+				var zipcode = reader.GetString(reader.GetOrdinal("Zipcode"));
+				var phoneNumber = reader.GetString(reader.GetOrdinal("PhoneNumber"));
+				var email = reader.GetString(reader.GetOrdinal("Email"));
+				var birthday = reader.GetDateTime(reader.GetOrdinal("Birthday"));
+
+				var majorid = -1; // means that the MajorId in the db is NULL
+				if(!reader.GetValue(reader.GetOrdinal("Majorid")).Equals(DBNull.Value)) {
+					majorid = reader.GetInt32(reader.GetOrdinal("MajorId"));
+				}
+
+				var sat = reader.GetInt32(reader.GetOrdinal("SAT"));
+				var gpa = reader.GetDouble(reader.GetOrdinal("GPA"));
+
+				// need to do the rest of the student database data
+				Student student = new Student();
+				student.Id = id;
+				student.FirstName = firstName;
+				student.LastName = lastName;
+				student.Address = address;
+				student.City = city;
+				student.State = state;
+				student.Zipcode = zipcode;
+				student.PhoneNumber = phoneNumber;
+				student.Email = email;
+				student.MajorId = majorid;
+				student.Birthday = birthday;
+				student.SAT = sat;
+				student.GPA = gpa;
+				students.Add(student);
+			}
+			reader.Close();
+			connection.Close();
+			return students;
+		}
+		public static Student Select(int id) {
+			return null;
+		}
+		public static bool Insert(Student student) {
+			return false;
+		}
+		public static bool Update(Student student) {
+			return false;
+		}
+		public static bool Delete(int id) {
+			return false;
+		}
+	}
+}
---
+++ TestEducationLibrary/Program.cs
@@ -0,0 +1,21 @@
+﻿using EducationLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEducationLibrary {
+
+	class Program {
+
+		void Run() {
+			StudentCollection students = StudentCollection.Select();
+			
+		}
+
+		static void Main(string[] args) {
+			new Program().Run();
+		}
+	}
+}
